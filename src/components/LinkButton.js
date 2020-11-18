00db35692ca9925cc02109d2d549b171a5e69fca
Vic <victor@webcoda.com.au>
2020-11-18 18:30:31 +1100

    [BH-100][BH-103][BH-104] 1. all buttons should animate on hover same as icon animations. Check reference as marked up on Figma file'Learn more' button 2. button prop

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -11,25 +11,33 @@ const LinkButton = ({
 	tag,
 	...props
 }) => {
-	let _className = `inline-block py-3 px-7 bg-yellow uppercase text-center text-inherit hocus:no-underline hocus:text-yellow hocus:bg-black transition duration-300 c-linkbutton ${className}`
+	let _className = `relative overflow-hidden inline-block py-3 px-7 bg-yellow uppercase text-center text-inherit hocus:no-underline transition duration-300 group c-linkbutton ${className}`
 	const Component = tag;
 	return tag ? (
-		<Component
-			{...props}
-			className={_className}
-		>
-			{ children }
-		</Component>
-	)
-	: (
-		<Link
-			to={to}
-			target={target}
-			className={_className}
-		>
+		<Component {...props} className={_className}>
 			{children}
+			<div
+				className="absolute inset-0 flex flex-col translate-y-full group-hocus:translate-y-0 transition duration-500 overflow-hidden"
+				aria-hidden="true"
+			>
+				{children}
+			</div>
+		</Component>
+	) : (
+		<Link to={to} target={target} className={_className}>
+			{children}
+			<div
+				className="absolute inset-0 flex flex-col translate-y-full group-hocus:translate-y-0 transition duration-500 overflow-hidden"
+				aria-hidden="true"
+			>
+				<div
+					className={`flex-1 py-3 px-7 -translate-y-full group-hocus:translate-y-0 transition duration-500 bg-black text-yellow`}
+				>
+					{children}
+				</div>
+			</div>
 		</Link>
-	);
+	)
 }
 
 export default LinkButton;
