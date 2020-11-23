b201a2895c8362090cc434fac3dfcfad8f513794
Vic <victor@webcoda.com.au>
2020-11-23 12:15:53 +1100

    [BH-123] On hover I see show more twice

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -3,6 +3,22 @@ import Link from '@/components/Link'
 
 import './LinkButton.scss'
 
+const LinkButtonInner = ({ children }) => (
+	<>
+		{children}
+		<div
+			className="absolute inset-0 flex flex-col translate-y-full group-hocus:translate-y-0 transition duration-500 overflow-hidden"
+			aria-hidden="true"
+		>
+			<div
+				className={`flex-1 py-3 px-7 -translate-y-full group-hocus:translate-y-0 transition duration-500 bg-black text-yellow`}
+			>
+				{children}
+			</div>
+		</div>
+	</>
+)
+
 const LinkButton = ({
 	className='',
 	children,
@@ -15,27 +31,11 @@ const LinkButton = ({
 	const Component = tag;
 	return tag ? (
 		<Component {...props} className={_className}>
-			{children}
-			<div
-				className="absolute inset-0 flex flex-col translate-y-full group-hocus:translate-y-0 transition duration-500 overflow-hidden"
-				aria-hidden="true"
-			>
-				{children}
-			</div>
+			<LinkButtonInner>{children}</LinkButtonInner>
 		</Component>
 	) : (
 		<Link to={to} target={target} className={_className}>
-			{children}
-			<div
-				className="absolute inset-0 flex flex-col translate-y-full group-hocus:translate-y-0 transition duration-500 overflow-hidden"
-				aria-hidden="true"
-			>
-				<div
-					className={`flex-1 py-3 px-7 -translate-y-full group-hocus:translate-y-0 transition duration-500 bg-black text-yellow`}
-				>
-					{children}
-				</div>
-			</div>
+			<LinkButtonInner>{children}</LinkButtonInner>
 		</Link>
 	)
 }
