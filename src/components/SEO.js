9f98435354189bcf257560577c1264423f6afd1d
Vic <victor@webcoda.com.au>
2020-11-30 12:56:47 +1100

    Update the title using the Global Header's siteName

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from 'gatsby'
 
 const SEO = ({ pageData }) => {
+	const { agilityGlobalHeader } = useStaticQuery(graphql`
+		query GlobalHeaderQueryForSEO {
+			agilityGlobalHeader(
+				properties: { referenceName: { eq: "globalheader" } }
+			) {
+				customFields {
+					siteName
+				}
+			}
+		}
+	`)
+
 	if(!pageData) return null;
 
 	const { title, scripts, seo } = pageData
 
     return (
 		<Helmet
-			title={`${title}  - Investa Parkline`}
+			title={`${title}  - ${agilityGlobalHeader.customFields.siteName}`}
 			meta={[
 				{
 					name: `description`,
@@ -16,16 +29,12 @@ const SEO = ({ pageData }) => {
 				},
 			]}
 		>
-			{
-				!!scripts.top && (
-					<div dangerouslySetInnerHTML={{ __html: scripts.top }} />
-				)
-			}
-			{
-				!!seo.metaHTML && (
-					<div dangerouslySetInnerHTML={{ __html: seo.metaHTML }} />
-				)
-			}
+			{!!scripts.top && (
+				<div dangerouslySetInnerHTML={{ __html: scripts.top }} />
+			)}
+			{!!seo.metaHTML && (
+				<div dangerouslySetInnerHTML={{ __html: seo.metaHTML }} />
+			)}
 		</Helmet>
 	)
 }
