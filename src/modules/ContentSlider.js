3295525b7414f860a9d99c9e708efabf88c120a4
Vic <victor@webcoda.com.au>
2020-12-17 11:08:58 +1100

    update content slider

--- src/modules/ContentSlider.js
+++ src/modules/ContentSlider.js
@@ -41,96 +41,113 @@ export default ({ item }) => {
 	// purgecss: .bg-yellow, .bg-grey-light
 	return (
 		<div className="c-contentslider mb-25 last:mb-0" data-aos="fade-up">
-			<CommonContainer>
-				<div
-					className={`bg-${backgroundColor} px-2.5 c-contentslider__top text-center ${
-						item.customFields.title ? 'pt-9 md:pt-23' : ''
-					}`}
-				>
-					<div className="row justify-center">
-						<div className="relative px-2.5 w-full sm:max-w-4/5 md:max-w-2/5">
-							{!!item.customFields.title && (
-								<>
-									<h2
-										dangerouslySetInnerHTML={renderHTML(
-											item.customFields.title.replace(/(?:\r\n|\r|\n)/g, '<br>')
-										)}
-									></h2>
-									<SmallDivider className="mt-7.5 mb-9.5 md:my-11" />
-								</>
-							)}
+			<div className="container-fluid">
+				<div className="row justify-center">
+					<div className="col-8">
+						<div
+							className={`bg-${backgroundColor} px-2.5 c-contentslider__top text-center ${
+								item.customFields.title ? 'pt-9 md:pt-23 has-title' : ''
+							}`}
+						>
+							<div className="row justify-center">
+								<div className="relative px-2.5 w-full sm:max-w-4/5 md:max-w-2/5">
+									{!!item.customFields.title && (
+										<>
+											<h2
+												dangerouslySetInnerHTML={renderHTML(
+													item.customFields.title.replace(
+														/(?:\r\n|\r|\n)/g,
+														'<br>'
+													)
+												)}
+											></h2>
+											<SmallDivider className="mt-7.5 mb-9.5 md:my-11" />
+										</>
+									)}
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</CommonContainer>
-			<div className={`c-contentslider__media ${isSingleVideo ? 'c-contentslider__media--singlevideo' : ''}`}>
-				<Swiper
-					className="h-full"
-					pagination={
-						hasMoreThanOneSlide ? { clickable: true } : false
-					}
-					navigation={hasMoreThanOneSlide}
-					loop={hasMoreThanOneSlide}
-					allowTouchMove={hasMoreThanOneSlide}
-				>
-					{media.map((mediaItem, index) => {
-						const mediaItemUrl = encodeUrl(mediaItem.url)
-
-						const mediaImgSources = [
-							{
-								srcset: [
-									{
-										src:
-											mediaItemUrl + '?q=80&w=2560',
-										descriptor: '2560w',
-									},
-									{
-										src:
-											mediaItemUrl + '?q=80&w=1920',
-										descriptor: '1920w',
-									},
-									{
-										src: mediaItemUrl + '?q=80&w=1024',
-										descriptor: '1024w',
-									},
-									{
-										src: mediaItemUrl + '?q=80&w=768',
-										descriptor: '768w',
-									},
-									{
-										src: mediaItemUrl + '?q=80&w=480',
-										descriptor: '480w',
-									},
-								],
-								type: 'image/png',
-							},
-						]
-
-						return (
-							<SwiperSlide
-								key={`content-slide-${item.contentId}-${index}`}
-							>
-								{!!mediaItem.url.match(/\.mp4/) ? (
-									<Plyr
-										type="video" // or "vimeo"
-										url={mediaItemUrl}
-										poster={poster}
-									/>
-								) : (
-									<BaseImg
-										src={mediaItemUrl + '?q=80&w=2560'}
-										lqipSrc={mediaItemUrl + '?q=80&w=8'}
-										sources={mediaImgSources}
-									></BaseImg>
-								)}
-							</SwiperSlide>
-						)
-					})}
-				</Swiper>
 			</div>
 			<CommonContainer>
-				<div className={`bg-${backgroundColor} h-11`}></div>
+				<div
+					className={`c-contentslider__media ${
+						isSingleVideo
+							? 'c-contentslider__media--singlevideo'
+							: ''
+					}`}
+				>
+					<Swiper
+						className="h-full"
+						pagination={
+							hasMoreThanOneSlide ? { clickable: true } : false
+						}
+						navigation={hasMoreThanOneSlide}
+						loop={hasMoreThanOneSlide}
+						allowTouchMove={hasMoreThanOneSlide}
+					>
+						{media.map((mediaItem, index) => {
+							const mediaItemUrl = encodeUrl(mediaItem.url)
+
+							const mediaImgSources = [
+								{
+									srcset: [
+										{
+											src: mediaItemUrl + '?q=80&w=2560',
+											descriptor: '2560w',
+										},
+										{
+											src: mediaItemUrl + '?q=80&w=1920',
+											descriptor: '1920w',
+										},
+										{
+											src: mediaItemUrl + '?q=80&w=1024',
+											descriptor: '1024w',
+										},
+										{
+											src: mediaItemUrl + '?q=80&w=768',
+											descriptor: '768w',
+										},
+										{
+											src: mediaItemUrl + '?q=80&w=480',
+											descriptor: '480w',
+										},
+									],
+									type: 'image/png',
+								},
+							]
+
+							return (
+								<SwiperSlide
+									key={`content-slide-${item.contentId}-${index}`}
+								>
+									{!!mediaItem.url.match(/\.mp4/) ? (
+										<Plyr
+											type="video" // or "vimeo"
+											url={mediaItemUrl}
+											poster={poster}
+										/>
+									) : (
+										<BaseImg
+											src={mediaItemUrl + '?q=80&w=2560'}
+											lqipSrc={mediaItemUrl + '?q=80&w=8'}
+											sources={mediaImgSources}
+										></BaseImg>
+									)}
+								</SwiperSlide>
+							)
+						})}
+					</Swiper>
+				</div>
 			</CommonContainer>
+			<div className="container-fluid">
+				<div className="row justify-center">
+					<div className="col-8">
+						<div className={`bg-${backgroundColor} h-11`}></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
