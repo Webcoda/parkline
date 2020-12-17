7ae5b90165075a53f37526ce017f5b5c2d3dc7d0
Vic <victor@webcoda.com.au>
2020-12-17 11:26:51 +1100

    update content hero

--- src/modules/ContentHero.js
+++ src/modules/ContentHero.js
@@ -78,16 +78,12 @@ const ContentHero = ({ item }) => {
 					descriptor: '480w',
 				},
 			],
-			type: 'image/png',
 		},
 	]
 
 	return (
 		<div className="relative mb-12.5 c-contenthero">
-			<div
-				className="relative overflow-hidden bg-black flex flex-col justify-end text-white"
-				style={{ height: 652 }}
-			>
+			<div className="relative overflow-hidden bg-black flex flex-col justify-end text-white c-contenthero__inner">
 				{!!mediaUrl && (
 					<div className="u-embed__item">
 						<BaseImg
@@ -98,42 +94,43 @@ const ContentHero = ({ item }) => {
 						<div className="u-embed__item u-scrim-hero"></div>
 					</div>
 				)}
-
-				<div className="relative container-fluid flex-auto flex flex-col justify-end pb-20 md:pb-50">
-					<div className="row justify-center c-contenthero__row">
-						<div className="md:col-10 c-contenthero__row-inner">
-							<div className="row">
-								<div className="relative px-2.5 md:max-w-7/10">
-									<h1
-										className={`h2 sm:h1 c-contenthero__title ${
-											toBool(isTitleBig)
-												? 'c-contenthero__title--big'
-												: ''
-										} ${
-											toBool(isTitleYellow)
-												? 'text-yellow'
-												: ''
-										}`}
-										data-aos="fade-up"
-									>
-										{title}
-									</h1>
-								</div>
-							</div>
-							{!!text && (
-								<div className="row mt-11">
-									<div
-										className="relative px-2.5 flex-grow md:max-w-2/5 md:ml-1/10"
-										data-aos="fade-up"
-										data-aos-delay="500"
-									>
-										<Richtext
-											className="lead pl-10 md:pl-0"
-											html={text}
-										/>
+				<div className="u-embed__item flex-auto flex flex-col">
+					<div className="relative container-fluid flex-auto flex flex-col justify-end pb-20 c-contenthero__textcontainer">
+						<div className="row justify-center c-contenthero__row">
+							<div className="md:col-10 c-contenthero__row-inner">
+								<div className="row">
+									<div className="relative px-2.5 md:max-w-7/10">
+										<h1
+											className={`h2 sm:h1 c-contenthero__title ${
+												toBool(isTitleBig)
+													? 'c-contenthero__title--big'
+													: ''
+											} ${
+												toBool(isTitleYellow)
+													? 'text-yellow'
+													: ''
+											}`}
+											data-aos="fade-up"
+										>
+											{title}
+										</h1>
 									</div>
 								</div>
-							)}
+								{!!text && (
+									<div className="row mt-11">
+										<div
+											className="relative px-2.5 flex-grow md:max-w-2/5 md:ml-1/10"
+											data-aos="fade-up"
+											data-aos-delay="500"
+										>
+											<Richtext
+												className="lead pl-10 md:pl-0"
+												html={text}
+											/>
+										</div>
+									</div>
+								)}
+							</div>
 						</div>
 					</div>
 				</div>
@@ -143,7 +140,14 @@ const ContentHero = ({ item }) => {
 					toBool(isUseYellowHorizontalBar) ? '' : 'is-use-two-bar'
 				}`}
 			>
-				<div className={`row ${!toBool(isUseYellowHorizontalBar) && twoLinesPosition === 'right' ? 'justify-end' : ''}`}>
+				<div
+					className={`row ${
+						!toBool(isUseYellowHorizontalBar) &&
+						twoLinesPosition === 'right'
+							? 'justify-end'
+							: ''
+					}`}
+				>
 					{toBool(isUseYellowHorizontalBar) ? (
 						<div className="col-8">
 							<div className="bg-yellow h-7.5 w-full -ml-2.5"></div>
