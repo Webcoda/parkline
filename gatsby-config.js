387962bdf8a019485e7e5f2f2006f6ead4f80510
Victor <victor@webcoda.com.au>
2021-02-01 13:27:47 +1100

    try gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
 })
 
-console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV)
 console.log(process.env.AGILITY_API_KEY)
 console.log(process.env.AGILITY_API_ISPREVIEW)
 
 const path = require('path')
-const magicImporter = require("node-sass-magic-importer")
+const magicImporter = require('node-sass-magic-importer')
 
 //configure your agility plugin with environment variables so that
 //your agility api credentials stay secure
 const agilityConfig = {
-  guid: process.env.AGILITY_GUID,
-  apiKey: process.env.AGILITY_API_KEY,
-  isPreview: process.env.AGILITY_API_ISPREVIEW
+	guid: process.env.AGILITY_GUID,
+	apiKey: process.env.AGILITY_API_KEY,
+	isPreview: process.env.AGILITY_API_ISPREVIEW,
 }
 
 /**
@@ -193,5 +193,34 @@ module.exports = {
 		// 		},
 		// 	},
 		// },
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				// The property ID; the tracking code won't be generated without it
+				trackingId: 'G-RJ05ZBWY2Q',
+				// Defines where to place the tracking script - `true` in the head and `false` in the body
+				head: true,
+				// // Setting this parameter is optional
+				// anonymize: true,
+				// // Setting this parameter is also optional
+				// respectDNT: true,
+				// // Avoids sending pageview hits from custom paths
+				// exclude: ['/preview/**', '/do-not-track/me/too/'],
+				// // Delays sending pageview hits on route update (in milliseconds)
+				// pageTransitionDelay: 0,
+				// // Enables Google Optimize using your container Id
+				// optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+				// // Enables Google Optimize Experiment ID
+				// experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+				// // Set Variation ID. 0 for original 1,2,3....
+				// variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+				// // Defers execution of google analytics script after page load
+				// defer: false,
+				// // Any additional optional fields
+				// sampleRate: 5,
+				// siteSpeedSampleRate: 10,
+				// cookieDomain: 'example.com',
+			},
+		},
 	],
 }
