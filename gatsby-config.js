421cb443a53ea5bd3e45e68e23017cdc3300be33
Victor <victor@webcoda.com.au>
2022-12-08 16:27:32 +0700

    add gatsby-plugin-google-tagmanager-timeout

--- gatsby-config.js
+++ gatsby-config.js
@@ -193,11 +193,17 @@ module.exports = {
 		// 		},
 		// 	},
 		// },
+		// {
+		// 	resolve: `gatsby-plugin-google-gtag`,
+		// 	options: {
+		// 		// You can add multiple tracking ids and a pageview event will be fired for all of them.
+		// 		trackingIds: ['GTM-N88Z6B4'],
+		// 	},
+		// },
 		{
-			resolve: `gatsby-plugin-google-gtag`,
+			resolve: 'gatsby-plugin-google-tagmanager-timeout',
 			options: {
-				// You can add multiple tracking ids and a pageview event will be fired for all of them.
-				trackingIds: ['GTM-N88Z6B4'],
+				id: 'GTM-N88Z6B4',
 			},
 		},
 	],
